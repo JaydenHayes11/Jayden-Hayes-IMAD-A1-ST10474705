76a35316f48655b5a6155b651aeb33abbd886231
ST10474705 <ST10474705@vcconnect.edu.za>
2025-03-28 15:43:14 +0200

    Comments

--- app/src/main/java/za/co/varsitycollege/imad_a1_st10474705/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/imad_a1_st10474705/MainActivity.kt
@@ -15,22 +15,29 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        //To assign values to the different buttons and TextViews ID's
         val ResetPageButton = findViewById<Button>(R.id.ResetPageButton)
         val ShowSuggestionsButton = findViewById<Button>(R.id.ShowSuggestionsButton)
         val TimeOfDayText = findViewById<TextView>(R.id.TimeOfDayText)
         val DisplayTextView = findViewById<TextView>(R.id.DisplayTextView)
 
+        //To open the button to provide designated code
         ShowSuggestionsButton?.setOnClickListener {
 
+            //Code to Clear the DisplayTextView when button is clicked
             DisplayTextView.text = ""
 
+            //Creating a pop-up that'll show up when button is clicked to confirm button being clicked
             Toast.makeText(
                 this@MainActivity,
                 "Suggestions Shown", Toast.LENGTH_LONG
             ).show()
+
+            //Creating IF function to convert the users inputted words to assign a specified response to show up
             if (TimeOfDayText.text.toString() == ("Morning") || TimeOfDayText.text.toString() == ("morning")) {
                 DisplayTextView.setText("Avocado Toast, Boiled Eggs, Omelet")
             }
+            //use of else statement to display the response to the specified word of option
             else if (TimeOfDayText.text.toString() == ("Mid-Morning") || TimeOfDayText.text.toString() == ("mid-morning")) {
                 DisplayTextView.setText("Muffins, Bagels, Fruit")
             }
@@ -53,11 +60,14 @@ class MainActivity : AppCompatActivity() {
 
 
 
+        //Start of Reset button to revert the page back to start up
         ResetPageButton?.setOnClickListener {
 
+            //Code to clear the input textview and the display textview to provide the options as well in display textview
             TimeOfDayText.text = ""
             DisplayTextView.text = "Options for time of Day:Morning,Mid-Morning,Afternoon,Mid-Afternoon,Dinner,After-Dinner"
 
+            //to create a pop-up when the reset button is clicked to show it's been clicked
             Toast.makeText(
                 this@MainActivity,
                 "Page Reset", Toast.LENGTH_LONG
